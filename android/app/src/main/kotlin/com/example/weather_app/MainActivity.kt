8c0c273daf75e57ac075ca710c5f69185e64da61
Paolo Brugnola <p.brugnola@pmp-industries.com>
2025-02-06 14:39:16 +0100

    commti changes

--- android/app/src/main/kotlin/com/example/weather_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/weather_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.weather_app
+package com.example.weather
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/secrets.dart
+++
@@ -1,5 +0,0 @@
-const openWeatherAPIKey = 'Your api key here';
-
-/*
-    Get your api key from https://home.openweathermap.org/users/sign_up
-*/
